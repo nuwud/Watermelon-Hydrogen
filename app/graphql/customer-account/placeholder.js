71ab8985ec9d36a8f0abcea0feb4561553f1ba51
Patrick Allan Wood <nuwudorder@gmail.com>
2025-09-07 16:48:02 -0700

    chore(codegen): add JS placeholder for customer-account GraphQL pointer to unblock build

---
+++ app/graphql/customer-account/placeholder.js
@@ -0,0 +1,8 @@
+// Placeholder to satisfy codegen pointers for customer account GraphQL operations.
+// Remove this file when real customer-account operations are added.
+
+export const CUSTOMER_ACCOUNT_PLACEHOLDER = `#graphql
+  query CustomerAccountPlaceholder {
+    __typename
+  }
+`;
